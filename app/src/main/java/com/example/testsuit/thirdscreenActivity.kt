12e963355d73d1438db09b016db62259f6a8dfb2
The-RAR <A7118F1480@bangkit.academy>
2022-07-14 00:51:15 +0700

    adding thirdscreenActivity.kt and adjust

---
+++ app/src/main/java/com/example/testsuit/thirdscreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testsuit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class thirdscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thirdscreen)
+    }
+}
